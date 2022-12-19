9e6972715e58df0a9c8c67916a4094b8de55ef50
Elvis-Almeida-Mendes-Junior <103849339+Elvis-Almeida-Mendes-Junior@users.noreply.github.com>
2022-12-19 18:03:18 -0300

    error alert added

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function NumeroRandom(min, max) {
+    
     min = Math.ceil(min);
+    
     max = Math.floor(max);
+    
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
+
 let btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
+
     let Dado = document.getElementById('dado').value;
-    let res = document.querySelector('.res');
-    res.innerText = NumeroRandom(0,Number(Dado));
-})
+
+    if(Dado.length <= 0) {alert('Digite Um Valor')}
+
+    else{
+
+        let res = document.querySelector('.res');
+
+        res.innerText = NumeroRandom(0,Number(Dado));
+
+    }
+});
+
